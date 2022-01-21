e0114ea5f05b95037ae1963eb9540264b2729432
HiotyNi <82592176+HiotyNi@users.noreply.github.com>
2022-01-20 21:38:10 -0300

    Add files via upload

---
+++ Minimal Browser/Minimal Browser/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace F_WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tb_url = new System.Windows.Forms.TextBox();
+            this.btn_ir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 28);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(5);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(415, 274);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // tb_url
+            // 
+            this.tb_url.Location = new System.Drawing.Point(12, 3);
+            this.tb_url.Margin = new System.Windows.Forms.Padding(3, 3, 3, 5);
+            this.tb_url.Name = "tb_url";
+            this.tb_url.Size = new System.Drawing.Size(149, 20);
+            this.tb_url.TabIndex = 1;
+            this.tb_url.TextChanged += new System.EventHandler(this.tb_url_TextChanged);
+            this.tb_url.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tb_url_KeyDown);
+            // 
+            // btn_ir
+            // 
+            this.btn_ir.Location = new System.Drawing.Point(167, 3);
+            this.btn_ir.Name = "btn_ir";
+            this.btn_ir.Size = new System.Drawing.Size(75, 21);
+            this.btn_ir.TabIndex = 2;
+            this.btn_ir.Text = "Navegar";
+            this.btn_ir.UseVisualStyleBackColor = true;
+            this.btn_ir.Click += new System.EventHandler(this.btn_ir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Minimal Browser";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(415, 302);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_ir);
+            this.Controls.Add(this.tb_url);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Minimal Browser // Criado por O_Hioty#8332";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox tb_url;
+        private System.Windows.Forms.Button btn_ir;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Minimal Browser/Minimal Browser/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F_WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Navegar()
+        {
+            webBrowser1.Navigate(tb_url.Text);
+        }
+
+        private void btn_ir_Click(object sender, EventArgs e)
+        {
+            if (tb_url.Text != "")
+            {
+                Navegar();
+            }
+            else
+            {
+                MessageBox.Show("Digite uma Url");
+                    tb_url.Focus();
+            }
+            
+        }
+
+        private void tb_url_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                Navegar();
+            }
+        }
+
+        private void tb_url_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
